5e66e19c61757a0c8077938b9bdb0d6c56fe453a
gvaldez_IAR <gvaldez@iar.unlp.edu.ar>
2024-01-09 16:27:05 -0300

    init a tciii

--- test/data_scripts/adquire_serial.py
+++ test/data_scripts/adquire_serial.py
@@ -4,7 +4,7 @@ import re
 FILE_SAMPLE = ''
 PORT_SERIAL = '/dev/ttyACM0' 
 BAUDRATE = 115200 
-NAME_FILE= '../../../results/TC-III-ADC/TC-III-ADC-1.50.txt'
+NAME_FILE= '../../../results/TC-III-ADC/TC-III-ADC-3.30.txt'
 expresion_regular = r'^\d+(,\d+){0,2}\r\n$'
 
 def read_serial(objSerial,cmd_send):
